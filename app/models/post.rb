8409ce94448b4192415bb858c627c6709f817851
Naofal Mufid <valnovcc@gmail.com>
2020-05-27 10:44:45 +0700

    Controller Add Feature

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,24 @@
 class Post < ApplicationRecord
+  include Shared::Callbacks
+
   belongs_to :user
-  mount_uploader :attachment, AvatarUploader
+  acts_as_votable
+  acts_as_commentable
+
   include PublicActivity::Model
   tracked only: [:create], owner: Proc.new{ |controller, model| controller.current_user }
+
+  default_scope -> { order('created_at DESC')}
+
+  mount_uploader :attachment, AvatarUploader
+
+  validate_presence_of :content
+  validate_presence_of :user
+
+  auto_html_for :content do
+    image
+    youtube(width: 400, height: 250, autoplay: true)
+    link target: '_blank', rel: 'nofollow'
+    simple_format
+  end
 end
